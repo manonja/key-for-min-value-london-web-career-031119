69dbf70b4e2470715a6000c7eb5d0d12c95ae03b
manon jacquin <manon.jacquin@gmail.com>
2019-02-26 14:04:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   
   # check if name_hash isn't empty
   if name_hash == {}
-    nil 
+    return nil 
   else 
     name_hash.each do |key, value|
       if value < min_value
